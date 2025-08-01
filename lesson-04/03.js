13bbd81240d1e9b5807e537841139ca6239b5ef4
AlexanderS <ksellos99@gmail.com>
2025-08-01 12:55:08 +0300

    Added function to find common elements between 2 arrays and returning new array with this elements

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -16,8 +16,26 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
+function includesElement(array,element) {
+    for (let i = 0;i<array.length;i++){
+        if (array[i] === element){
+            return true;
+        }
 
-
-function findCommonElements() {
-
+    }
+    return false;
 }
+
+function findCommonElements(array1,array2) {
+    let newArray = [];
+    for (let i=0;i<array1.length;i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if(array1[i]===array2[j]){
+                newArray.push(array1[i]);
+            }
+        }
+    }
+    return newArray;
+}
+
+console.log(findCommonElements([1, 2, 3], [2, 3, 4]));
